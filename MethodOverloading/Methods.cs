0318eb83ce12429d56120680bc7bd5e764a399e1
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-26 11:34:28 -0700

    Completed Method Overloading

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool condition)
+        {
+            if (condition) 
+            {
+                if (num1 + num2 == 1)
+                {
+                    return Convert.ToString($"{num1 + num2} dollar");
+                }
+                else
+                {
+                    return Convert.ToString($"{num1 + num2} dollars");
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Methods.Add(1,0,true));
         }
     }
 }
